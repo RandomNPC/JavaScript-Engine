ea7d763f60ddbf9b59d08e3c8b469db67e7d65f1
Edward Thang <RandomClown@gmail.com>
2013-09-02 01:06:02 -0700

    Sample step & draw loop

--- js/Main.js
+++ js/Main.js
@@ -2,6 +2,18 @@
 var canvas, ctx;
 var mouse={ x: 0, y: 0 };
 
+var cat;
+
 
 window.onload=function() { // Makes sure the website is loaded before running code
+	canvas=document.getElementById('canvas');
+	ctx=canvas.getContext('2d');
+
+
+	cat=new Texture('nyanCat', { x: 64, y: 64 });
+	cat.face(5.5);
+	setInterval(function() {
+		cat.step();
+		cat.draw(ctx, { x: 20, y: 20 });
+	}, 1000/60);
 };
